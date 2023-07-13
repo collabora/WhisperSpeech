a643831ce5f479a7f61c4089b0977c42c6b64d41
Jakub Piotr Cłapa <jpc@collabora.com>
2023-07-13 13:04:27 +0000

    Implemented model loading and inference methods for the quantization model

--- spear_tts_pytorch/extract_stoks.py
+++ spear_tts_pytorch/extract_stoks.py
@@ -18,6 +18,10 @@ import numpy as np
 import pylab as plt
 import pandas as pd
 
+from huggingface_hub import hf_hub_download
+from fastcore.basics import store_attr
+import torch.nn.functional as F
+
 from fastcore.script import *
 
 # %% ../nbs/2F. Residual (RQ) semantic token extraction model.ipynb 27
@@ -50,12 +54,11 @@ def init_transformer(m):
 # %% ../nbs/2F. Residual (RQ) semantic token extraction model.ipynb 28
 class RQBottleneckTransformer(nn.Module):
     def __init__(self, width=384, vq_codes=512, q_depth=12, depth=1, n_head=2,
-                 codebook_dim=2, threshold_ema_dead_code=2, use_cosine_sim = False, kl_loss_mul=1):
-        super(RQBottleneckTransformer, self).__init__()
+                 codebook_dim=2, threshold_ema_dead_code=2, use_cosine_sim = False, kl_loss_mul=1,
+                 whisper_model_name='tiny.en'):
+        super().__init__()
+        store_attr("codebook_dim,vq_codes,q_depth,n_head,depth,use_cosine_sim,whisper_model_name")
         
-        self.whisper_decoder_config = whisper.DecodingOptions(language='en')
-        
-        self.vq_codes = vq_codes
         self.kl_loss_mul = kl_loss_mul
         
         self.in_blocks = nn.Sequential(*[
@@ -78,19 +81,20 @@ class RQBottleneckTransformer(nn.Module):
         self.kl_lossf = nn.KLDivLoss(reduction='batchmean')
 
         self.register_buffer("positional_embedding", sinusoids(1500, width))
+        self.register_buffer("embs_padding", None)
         
         self.out_blocks = nn.Sequential(*[
             whisper.model.ResidualAttentionBlock(width, n_head) for _ in range(depth)
         ])
         self.ln_post = LayerNorm(width)
         
+        self.whmodel = None
+        
         self.apply(init_transformer)
-    
-    def encode(self, embs):
-        x = self.ln_vq(self.in_blocks(embs))
-        _, indices, _ = self.rq(x)
-        return indices
 
+    #
+    # training
+    #
     @torch.no_grad()
     def get_teacher_logits(self, embs, input_toks, output_toks):
         teacher_logits = whmodel.decoder(input_toks, embs)
@@ -113,6 +117,71 @@ class RQBottleneckTransformer(nn.Module):
         self.kl_loss = self.kl_lossf(F.log_softmax(logits, dim=-1), F.softmax(teacher_logits, dim=-1))
         loss = self.ce_loss + self.kl_loss_mul * self.kl_loss + self.commit_loss
         return x, loss
+    
+    #
+    # inference
+    #
+    @classmethod
+    def load_model(cls, repo_id="collabora/spear-tts-pytorch", filename="whisper-vq-stoks.model", local_filename=None):
+        if not local_filename:
+            local_filename = hf_hub_download(repo_id=repo_id, filename=filename)
+        spec = torch.load(local_filename) 
+        vqmodel = cls(**spec['config'])
+        vqmodel.load_state_dict(spec['state_dict'])
+        vqmodel.eval()
+        return vqmodel
+    
+    def save_model(self, fname):
+        torch.save(dict(config = self.__stored_args__, state_dict = self.state_dict()), fname)
+        
+    def ensure_whisper(self):
+        assert not self.training
+        if self.whmodel is None: self.whmodel = whisper.load_model(self.whisper_model_name)
+        assert self.whisper_model_name.endswith('.en'), "multilingual models are not supported right now"
+        self.decoding_options = whisper.DecodingOptions(language='en')
+        self.tokenizer = whisper.tokenizer.get_tokenizer(False, language='en')
+        silent_mel = whisper.log_mel_spectrogram(torch.zeros((1,16000*30)))
+        self.embs_padding = self.whmodel.encoder(silent_mel.to(self.whmodel.device))
+    
+    def quantize(self, embs):
+        x = self.ln_vq(self.in_blocks(embs))
+        _, stoks, _ = self.rq(x)
+        if self.q_depth == 1:
+            stoks = stoks.squeeze(-1)
+        return stoks
+
+    def dequantize(self, stoks):
+        assert self.q_depth == 1
+        assert len(stoks.shape) == 1, "batch processing is not supported"
+        if isinstance(stoks, np.ndarray): stoks = torch.tensor(stoks)
+        # remove padding
+        padding = torch.nonzero(stoks == 1024)
+        if padding.any(): stoks = stoks[:padding[0,0]]
+        x = self.rq.layers[0]._codebook.embed[0,stoks.to(torch.long).view(-1)]
+        x = F.pad(x, (0, 0, 0, 1500-len(x)))
+        x = (self.rq.layers[0].project_out(x) + self.positional_embedding).unsqueeze(0)
+        return self.ln_post(self.out_blocks(x))
+
+    def encode_audio(self, audio):
+        if isinstance(audio, str):
+            x, sr = torchaudio.load(audio)
+            x = torchaudio.transforms.Resample(sr, 16000)(x)[0]
+            audio = x.unsqueeze(0)
+        return self.encode_mel(whisper.log_mel_spectrogram(audio))
+    
+    def encode_mel(self, mel):
+        assert len(mel.shape) == 3, "invalid mel spectrogram shape, expect (batch,chn,time)"
+        self.ensure_whisper()
+        padded = whisper.audio.pad_or_trim(mel, whisper.audio.N_FRAMES)
+        embs = self.whmodel.encoder(padded.to(self.whmodel.device))
+        return self.quantize(embs)
+    
+    def decode_text(self, stoks, decoding_options=None):
+        self.ensure_whisper()
+        if decoding_options is None: decoding_options = self.decoding_options
+        embs = self.dequantize(stoks).to(self.whmodel.device)
+        embs = torch.cat([embs, self.embs_padding[:,embs.shape[1]:]], dim=1)
+        return self.whmodel.decode(embs, decoding_options)
 
 # %% ../nbs/2F. Residual (RQ) semantic token extraction model.ipynb 38
 def load_model(fname='./vqmodel2-tiny-1000h.pth'):
