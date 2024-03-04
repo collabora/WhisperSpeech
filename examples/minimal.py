130bc1937a024ab0cd61af24dcaedec8d20a2305
BBC-Esq <bbc@chintellalaw.com>
2024-03-04 15:12:21 -0500

    Update minimal.py
    Add commented lines to show how to easily test different models.

--- examples/minimal.py
+++ examples/minimal.py
@@ -1,6 +1,8 @@
 from whisperspeech.pipeline import Pipeline
 
-tts_pipe = Pipeline(s2a_ref='collabora/whisperspeech:s2a-q4-tiny-en+pl.model')
+tts_pipe = Pipeline(s2a_ref='collabora/whisperspeech:s2a-q4-tiny-en+pl.model') # uncomment the line for the model you want to use
+# tts_pipe = Pipeline(s2a_ref='collabora/whisperspeech:s2a-q4-base-en+pl.model') # uncomment the line for the model you want to use
+# tts_pipe = Pipeline(s2a_ref='collabora/whisperspeech:s2a-q4-small-en+pl.model') # uncomment the line for the model you want to use
 
 save_path = 'output.wav'
 tts_pipe.generate_to_file(save_path, "This is a test") 
