0a4de68e965d33094d49df5f47e00b48d5493321
BBC-Esq <bbc@chintellalaw.com>
2024-03-07 06:39:24 -0500

    clarify available file formats to save in
    Clarify that a user can specify a different file format and torchaudio's backend ```soundfile``` will automatically save it.

--- examples/minimal.py
+++ examples/minimal.py
@@ -4,5 +4,5 @@ tts_pipe = Pipeline(s2a_ref='collabora/whisperspeech:s2a-q4-tiny-en+pl.model') #
 # tts_pipe = Pipeline(s2a_ref='collabora/whisperspeech:s2a-q4-base-en+pl.model') # uncomment the line for the model you want to use
 # tts_pipe = Pipeline(s2a_ref='collabora/whisperspeech:s2a-q4-small-en+pl.model') # uncomment the line for the model you want to use
 
-save_path = 'output.wav'
+save_path = 'output.wav' # change the file extension to .mp3, .flac, .ogg etc. to save to a different file format
 tts_pipe.generate_to_file(save_path, "This is a test") 
